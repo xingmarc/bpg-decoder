7cff07e4dc5bb285c9a2d5d882e248c07fbf64f7
Yichi Xing <Xing@Yichis-MacBook-Pro.local>
2017-03-03 19:28:31 -0800

    Add index.js and package.json

---
+++ index.js
@@ -0,0 +1,9 @@
+var BPGDecoder = require('./bpgdec.js')
+var BPGDecoder8 = require('./bpgdec8.js')
+var BPGDecoder8a = require('./bpgdec8a.js')
+
+module.exports = {
+	BPGDecoder: BPGDecoder,
+	BPGDecoder8: BPGDecoder8,
+	BPGDecoder8a: BPGDecoder8a
+}
