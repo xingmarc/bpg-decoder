cbb367f6c711461d915d84c3fe0a5b04cfc71704
xingmarc <yichixing@gmail.com>
2017-03-22 16:27:31 -0700

    Babilify all files.

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+var BPGDecoder=require('./bpgdec.js'),BPGDecoder8=require('./bpgdec8.js'),BPGDecoder8a=require('./bpgdec8a.js');module.exports={BPGDecoder:BPGDecoder,BPGDecoder8:BPGDecoder8,BPGDecoder8a:BPGDecoder8a};
